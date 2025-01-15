3ce8be2c611441d3deb540c6cff909dc23287ac6
livealltim <vinaysocialmedia2526@gmail.com>
2025-01-15 17:52:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ document.getElementById('userForm').addEventListener('submit', async function (e
 
   // Send data to backend API
   try {
-    const response = await fetch('https://ba-6.onrender.com', {
+    const response = await fetch('https://ba-6.onrender.com/saveData', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
